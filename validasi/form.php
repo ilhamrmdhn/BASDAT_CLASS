3188b03cd172adabdf875e4ea3ccf5d90a386f53
ilhamrmdhn <ilham712svr@gmail.com>
2022-11-23 09:37:59 +0700

    commit

--- validasi/form.php
+++ validasi/form.php
@@ -1,3 +1,10 @@
+<?php
+
+    $time = (object) $_GET['time'];
+    $errors = (object) $_GET['errors'];
+
+?>
+
 <html>
     <head>
         <title>Validasi Form</title>
@@ -6,19 +13,25 @@
         <form action="proses.php">
             <div>
                 <label>Nama (Harus input nama)</label><br>
-                <input type="text" name="nama">
+                <input type="text" value="<?php echo $time->nama?>" name="nama">
+                <?php
+                if($errors->$nama):?>
+                <div style="color: red"><?php echo $errors->nama[0]?></div>
+                <?php
+                endif;
+                ?>
             </div>
             <div style="margin-top:10px;">
                 <label>Email (Harus input @)</label><br>
-                <input type="text" name="email">
+                <input type="text" value="<?php echo $time->email?>" name="email">
             </div>
             <div style="margin-top:10px;">
                 <label>Username (tidak boleh regex)</label><br>
-                <input type="text" name="username">
+                <input type="text" value="<?php echo $time->username?>" name="username">
             </div>
             <div style="margin-top:10px;">
                 <label>Usia (Harus input angka)</label><br>
-                <input type="text" name="usia">
+                <input type="text value="<?php echo $time->usia?>"" name="usia">
             </div>
             <div style="margin-top:10px;">
                 <button>Submit</button>
--- validasi/proses.php
+++ validasi/proses.php
@@ -14,8 +14,22 @@ $rules = [
 
 $errors = validasi($rules);
 
-echo "<pre>";
-print_r($errors);
-echo "</pre>";
+// echo "<pre>";
+// print_r($errors);
+// echo "</pre>";
+
+// disini kita bisa melakukan proses yang harus dilakukan
+// jika tidak terjadi error validasi apapun
+
+if(count($errors) > 0){
+    $time = $_REQUEST;
+    $queryString = http_build_query([
+        'errors' => $errors,
+        'time' => $time
+    ]);
+
+    header("location: form.php?($queryString)");
+    die();
+}
 
 ?>
